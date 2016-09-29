d1595311f3f76092be7171d4e491ea176c376b3d
Nick Dobie <dobienick@gmail.com>
2016-09-29 14:33:14 -0500

    Initial commit

---
+++ src/launcher.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import fs from 'fs-extra';
+import Log from 'log';
+import morgan from 'morgan';
+import path from 'path';
+
+const DEFAULT_LOG_NAME = 'static-server.txt';
+
+export default class StaticServerLauncher {
+  onPrepare({ staticServerFolders: folders, staticServerLog: logging = false,
+      staticServerPort: port = 4567 }) {
+    if (!folders) {
+      return Promise.resolve();
+    }
+
+    this.server = express();
+    this.folders = folders;
+    this.port = port;
+
+    if (logging) {
+      let stream;
+      if (typeof logging === 'string') {
+        const file = path.join(logging, DEFAULT_LOG_NAME);
+        fs.createFileSync(file);
+        stream = fs.createWriteStream(file);
+      }
+      this.log = new Log('debug', stream);
+      this.server.use(morgan('tiny', { stream }));
+    } else {
+      this.log = new Log('emergency');
+    }
+
+    (Array.isArray(folders) ? folders : [ folders ]).forEach((folder) => {
+      this.log.debug('Mounting folder `%s` at `%s`', path.resolve(folder.path), folder.mount);
+      this.server.use(folder.mount, express.static(folder.path));
+    });
+
+    return new Promise((resolve, reject) => {
+      this.server.listen(this.port, (err) => {
+        if (err) {
+          reject(err);
+        }
+
+        this.log.info(`Static server running at http://localhost:${port}`);
+        resolve();
+      });
+    });
+  }
+
+}
